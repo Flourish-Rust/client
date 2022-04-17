490696125c53d51d698a1ff595c9bd29263ae30b
Jordan Baron <jordannbaron@gmail.com>
2022-04-17 00:13:42 -0400

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<div data-tauri-drag-region class="titlebar">
+  <div class="titlebar-button" id="titlebar-minimize">
+    <img
+      src="https://api.iconify.design/mdi:window-minimize.svg"
+      alt="minimize"
+    />
+    test
+  </div>
+  <div class="titlebar-button" id="titlebar-close">
+    <img src="https://api.iconify.design/mdi:close.svg" alt="close" />
+  </div>
+</div>
