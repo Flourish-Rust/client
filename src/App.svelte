db860cd0e6026d9a5c61d3966e5124b81e11ac1d
Jordan Baron <jordannbaron@gmail.com>
2022-04-17 02:14:20 -0400

    initial UI

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,62 @@
-<div data-tauri-drag-region class="titlebar">
-  <div class="titlebar-button" id="titlebar-minimize">
-    <img
-      src="https://api.iconify.design/mdi:window-minimize.svg"
-      alt="minimize"
-    />
-    test
+<script lang="ts">
+  import { open } from "@tauri-apps/api/dialog";
+  import Titlebar from "./lib/Titlebar.svelte";
+
+  let installDir = "C:/Users/jordan/Desktop/Rust/116";
+  let version = "";
+
+  const selectInstallDir = async () => {
+    await open();
+  };
+
+  const installRust = () => {};
+</script>
+
+<main class="w-full h-screen flex items-center justify-center font-quicksand">
+  <div class="w-full h-full bg-black bg-opacity-10">
+    <Titlebar />
+    <div class="h-full flex flex-col items-center justify-center">
+      <img src="logo.webp" alt="Flourish Rust Logo" width="128" />
+      <form
+        action="#"
+        class="flex flex-col w-1/2"
+        on:submit|preventDefault={installRust}
+      >
+        <label for="version" class="text-white mt-4">Version</label>
+        <select name="version" class="p-1" bind:value={version}>
+          <option value="legacy">Legacy</option>
+          <option value="devblog-116">Blueprints (Devblog 133)</option>
+          <option value="devblog-133">XP (Devblog 133)</option>
+        </select>
+        <label for="install-directory" class="text-white mt-4"
+          >Install Directory</label
+        >
+        <div class="flex w-full">
+          <span
+            name="install-directory"
+            class="rounded-l-md w-full bg-white p-2 overflow-hidden flex items-center"
+            >{installDir}</span
+          >
+          <button
+            class="bg-white rounded-r-md pl-4 pr-1"
+            on:click={selectInstallDir}
+            ><i class="bx bx-dots-horizontal-rounded" /></button
+          >
+        </div>
+        <button
+          type="submit"
+          class="bg-[#B7410E] hover:bg-[#94340b] transition-all px-6 py-2 text-white rounded-2xl font-bold text-lg mt-8"
+          >Install</button
+        >
+      </form>
+    </div>
   </div>
-  <div class="titlebar-button" id="titlebar-close">
-    <img src="https://api.iconify.design/mdi:close.svg" alt="close" />
-  </div>
-</div>
+</main>
+
+<style>
+  main {
+    background-image: url("assets/bg.webp");
+    background-size: cover;
+    background-position: center;
+  }
+</style>
---
+++ src/lib/Titlebar.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { appWindow } from "@tauri-apps/api/window";
+
+  const closeWindow = async () => {
+    await appWindow.close();
+  };
+
+  const minimizeWindow = async () => {
+    await appWindow.minimize();
+  };
+</script>
+
+<div
+  data-tauri-drag-region
+  class="titlebar flex absolute justify-end items-center w-full"
+>
+  <div class="titlebar-button" id="titlebar-minimize">
+    <button on:click={minimizeWindow}
+      ><i
+        class="bx bx-sm bx-minus text-white hover:bg-black bg-opacity-5 p-2 transition-all"
+      /></button
+    >
+    <button on:click={closeWindow}
+      ><i class="bx bx-sm bx-x text-white hover:bg-red-400 p-2" /></button
+    >
+  </div>
+</div>
